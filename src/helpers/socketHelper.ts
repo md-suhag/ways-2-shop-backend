629d2c88f4a670cdfc82e4002ded551ee16c96cb
md-suhag <abdussalamsuhag104@gmail.com>
2025-10-09 13:30:23 +0600

    update: user status change when connected/disconnected

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -1,16 +1,41 @@
 import colors from "colors";
 import { Server } from "socket.io";
 import { logger } from "../shared/logger";
+import { User } from "../app/modules/user/user.model";
 
-const socket = (io: Server)=>{
-    io.on('connection', socket=>{
-        logger.info(colors.blue('A User connected'));
+const socket = (io: Server) => {
+  io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId as string;
 
-        // disconnect
-        socket.on("disconnect", ()=>{
-            logger.info(colors.red('A user disconnect'));
-        })
-    })
-}
+    // disconnect if userId is not provided
+    if (!userId) {
+      socket.emit("error", "User ID is required");
+      socket.disconnect(true);
+      return;
+    }
+    // User is now online
+    logger.info(colors.blue("A user connected"));
+    User.findByIdAndUpdate(userId, { isOnline: true }).catch((err) => {
+      logger.error(
+        colors.red(`Error updating user online status: ${err.message}`)
+      );
+    });
+    io.emit("userStatus", { userId, isOnline: true });
 
-export const socketHelper = { socket }
+    //disconnect
+    socket.on("disconnect", () => {
+      logger.info(colors.red("A user disconnect"));
+      if (userId) {
+        // User is now offline
+        User.findByIdAndUpdate(userId, { isOnline: false }).catch((err) => {
+          logger.error(
+            colors.red(`Error updating user online status: ${err.message}`)
+          );
+        });
+        io.emit("userStatus", { userId, isOnline: false });
+      }
+    });
+  });
+};
+
+export const socketHelper = { socket };
