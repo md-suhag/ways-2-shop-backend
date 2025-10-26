84e3db5e35cd5c7e9a82677dc17345073fc0cb99
md-suhag <abdussalamsuhag104@gmail.com>
2025-10-27 03:51:20 +0600

    update: stripe api version

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,8 +1,8 @@
-import Stripe from 'stripe';
-import config from '.';
+import Stripe from "stripe";
+import config from ".";
 
 const stripe = new Stripe(config.stripe.stripeSecretKey as string, {
-    apiVersion: '2024-06-20',
+  apiVersion: "2025-09-30.clover",
 });
 
 export default stripe;
