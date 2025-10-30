fd7839b8e6a2d94e7b6596e23bb20c2476a40a09
md-suhag <abdussalamsuhag104@gmail.com>
2025-10-30 16:31:17 +0600

    feat: add maximum file size  validation

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,93 +1,96 @@
-import { ErrorRequestHandler } from 'express';
-import config from '../../config';
-import ApiError from '../../errors/ApiErrors';
-import handleValidationError from '../../errors/handleValidationError';
-import handleZodError from '../../errors/handleZodError';
-import { errorLogger } from '../../shared/logger';
-import { IErrorMessage } from '../../types/errors.types';
-import { StatusCodes } from 'http-status-codes';
+import { ErrorRequestHandler } from "express";
+import config from "../../config";
+import ApiError from "../../errors/ApiErrors";
+import handleValidationError from "../../errors/handleValidationError";
+import handleZodError from "../../errors/handleZodError";
+import { errorLogger } from "../../shared/logger";
+import { IErrorMessage } from "../../types/errors.types";
+import { StatusCodes } from "http-status-codes";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const globalErrorHandler: ErrorRequestHandler = (error, req, res,next) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    config.node_env === 'development'
-    ? console.log('🚨 globalErrorHandler', error)
-    : errorLogger.error('🚨 globalErrorHandler', error);
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  config.node_env === "development"
+    ? console.log("🚨 globalErrorHandler", error)
+    : errorLogger.error("🚨 globalErrorHandler", error);
 
-    let statusCode = 500;
-    let message = 'Something went wrong';
-    let errorMessages: IErrorMessage[] = [];
+  let statusCode = 500;
+  let message = "Something went wrong";
+  let errorMessages: IErrorMessage[] = [];
 
-    if (error.name === 'ZodError') {
-        const simplifiedError = handleZodError(error);
-        statusCode = simplifiedError.statusCode;
-        message = simplifiedError.message;
-        errorMessages = simplifiedError.errorMessages;
-    }else if (error.name === 'ValidationError') {
-        const simplifiedError = handleValidationError(error);
-        statusCode = simplifiedError.statusCode;
-        message = simplifiedError.message;
-        errorMessages = simplifiedError.errorMessages;
-    } else if (error.name === 'TokenExpiredError') {
-        statusCode = StatusCodes.UNAUTHORIZED
-        message = 'Session Expired'
-        errorMessages = error?.message
-            ? 
-            [
-                {
-                    path: '',
-                    message: 'Your session has expired. Please log in again to continue.',
-                }
-            ]
-            : 
-            []
-    }
-    else if (error.name === 'JsonWebTokenError') {
-        statusCode = StatusCodes.UNAUTHORIZED
-        message = 'Invalid Token'
-        errorMessages = error?.message
-            ? 
-            [
-                {
-                    path: '',
-                    message: 'Your token is invalid. Please log in again to continue.'
-                }
-            ]
-            : []
-    } 
-    else if (error instanceof ApiError) {
-        statusCode = error.statusCode;
-        message = error.message;
-        errorMessages = error.message
-            ? 
-            [
-                {
-                    path: '',
-                    message: error.message,
-                }
-            ]
-            : 
-            [];
-    } else if (error instanceof Error) {
-        message = error.message;
-        errorMessages = error.message
-            ? 
-            [
-                {
-                    path: '',
-                    message: error?.message,
-                }
-            ]
-            : 
-            [];
-    }
+  if (error.name === "ZodError") {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error.name === "ValidationError") {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error.name === "TokenExpiredError") {
+    statusCode = StatusCodes.UNAUTHORIZED;
+    message = "Session Expired";
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message:
+              "Your session has expired. Please log in again to continue.",
+          },
+        ]
+      : [];
+  } else if (error.name === "JsonWebTokenError") {
+    statusCode = StatusCodes.UNAUTHORIZED;
+    message = "Invalid Token";
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: "Your token is invalid. Please log in again to continue.",
+          },
+        ]
+      : [];
+  } else if (error.code === "LIMIT_FILE_SIZE") {
+    statusCode = StatusCodes.BAD_REQUEST;
+    message = "File too large. Maximum file size 10 MB";
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof ApiError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: "",
+            message: error.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
 
-    res.status(statusCode).json({
-        success: false,
-        message,
-        errorMessages,
-        stack: config.node_env !== 'production' ? error?.stack : undefined,
-    });
-}
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.node_env !== "production" ? error?.stack : undefined,
+  });
+};
 
 export default globalErrorHandler;
